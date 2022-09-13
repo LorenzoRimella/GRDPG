c463d4b8292dabffdc954eb2d0a5028cff9dd82e
LorenzoRimella <l.rimella@lancaster.ac.uk>
2022-09-13 10:54:42 +0100

    simulator for GRDPG and block model

---
+++ simulator.py
@@ -0,0 +1,48 @@
+import numpy as np
+import tensorflow as tf
+import tensorflow_probability as tfp
+
+class GRDPG:
+
+    def __init__(self, p, q, N):
+
+        self.p = p
+        self.q = q
+        self.I      = tf.concat((tf.ones(p), tf.ones(q)), axis = 0)
+        self.N      = N
+
+    def sample_Beta(self, beta_a, beta_b):
+
+        X      = tfp.distributions.Beta(beta_a, beta_b).sample((self.N, self.p+self.q))
+        Xminus = X*tf.reshape(self.I, (1, self.p+self.q))
+
+        M = tf.einsum("ik,jk->ij", Xminus, X)
+
+        M = tf.reduce_min(tf.concat((tf.reshape(M, (self.N, self.N, 1)), tf.ones( (self.N, self.N, 1))), axis =2), axis =2)
+        M = tf.reduce_max(tf.concat((tf.reshape(M, (self.N, self.N, 1)), tf.zeros((self.N, self.N, 1))), axis =2), axis =2)
+
+        A = tfp.distributions.Binomial(1, probs = np.triu(M, k = 1)).sample()
+
+        return A + tf.transpose(tf.experimental.numpy.triu(A, k = 1))
+
+
+class BlockModel:
+    
+    def __init__(self, B):
+
+        self.B = B
+
+    def pop_sample(self, initial_distribution, N):
+
+        Xrv = tfp.distributions.OneHotCategorical(probs = initial_distribution)
+
+        return Xrv.sample(N)
+
+    def sample(self, X):
+
+        M = np.einsum("nj,kj->nk", np.einsum("nk,kj->nj", X, self.B), X)
+
+        A = tfp.distributions.Binomial(1, probs = np.triu(M, k = 1)).sample()
+
+        return A + tf.transpose(tf.experimental.numpy.triu(A, k = 1))
+
